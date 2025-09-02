4ab4d0bc917254cab8b7fe295c083b33f59b32cf
Rick-twnus <tingxie49@gmail.com>
2025-09-02 20:12:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 // 目前首頁不需要特別JS，可保留空檔以後擴充
 console.log("首頁載入完成");
+function setFullHeight() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+window.addEventListener('resize', setFullHeight);
+window.addEventListener('load', setFullHeight);
+setFullHeight();
+
 
